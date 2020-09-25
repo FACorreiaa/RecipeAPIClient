1d47f173807076643eead5f4786668c70f093eea
FAcorreiaa <fernandocorreia316@gmail.com>
2020-09-25 22:32:58 +0100

    adding loader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import "./App.css";
-
-import ResourceList from "./components/ResourceList";
 import "bootstrap/dist/css/bootstrap.min.css";
-import CustomNav from "./components/navbar/CustomNav";
+import Loader from "react-loader-spinner";
 
+const ResourceComponent = lazy(() => import("./components/ResourceList"));
+const NavBar = lazy(() => '"./components/navbar/CustomNav"');
+const renderLoader = () => (
+  <Loader type="MutatingDots" color="#2BAD60" height="100" width="100" />
+);
 function App() {
   const [resource] = useState();
 
@@ -12,9 +15,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="App-title">
-          <CustomNav />
+          <NavBar />
         </div>
-        <ResourceList className="App-resource" resource={resource} />
+        <Suspense fallback={renderLoader()}>
+          <ResourceComponent className="App-resource" resource={resource} />
+        </Suspense>
       </header>
     </div>
   );
